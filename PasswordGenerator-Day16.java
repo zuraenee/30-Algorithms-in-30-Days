f0ea343a0fe9d28ecaf0f136f425af469bd99c50
Ananda Zuraenee Sudarmaji <97554212+zuraenee@users.noreply.github.com>
2023-02-04 23:50:05 +0000

    Update PasswordGenerator-Day16.java

--- PasswordGenerator-Day16.java
+++ PasswordGenerator-Day16.java
@@ -24,7 +24,7 @@ public class passwordGenerator {
         int randomCharacterNum = random.nextInt(3);
         int[] number = {1,2,3,4,5,6,7,8,9};
         int randomIndex = 0;
-        String[] symbols = {"!","@","#","$","%","^","&","*","(",")","_","-","=","+","<",">","?","/","|",";",":"};
+        String[] symbols = {"!","@","#","$","%","^","&","*","(",")","_","-","=","+","<",">","?","/","|",";",":","{","}";
         String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
         String[] alphabetUpper = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
         if (randomCharacterNum == 0){
