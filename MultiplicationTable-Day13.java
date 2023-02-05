7c1b5b5647efde5c9dad6ade92757e797c1d1765
Ananda Zuraenee Sudarmaji <97554212+zuraenee@users.noreply.github.com>
2023-02-05 23:56:48 +0000

    Update MultiplicationTable-Day13.java

--- MultiplicationTable-Day13.java
+++ MultiplicationTable-Day13.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class MultiplicationTable {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter multiplication table size: ");
+            System.out.print("Enter multiplication table size : ");
             int x = scanner.nextInt();
             for(int i = 1; i <= x; ++i) {
                 for(int j = 1; j <= i; ++j) {
