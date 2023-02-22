c45640ba334e324739325ad7a07e17149ea245c7
Ananda Zuraenee Sudarmaji <97554212+zuraenee@users.noreply.github.com>
2023-02-22 22:20:27 +0000

    Create AsciiValue-Day18.cpp

---
+++ AsciiValue-Day18.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string c;
+    int totalValue;
+    cout << "Enter a String : ";
+    cin >> c;
+    for (int i=0; i<c.length() ; i++){
+
+        totalValue += int(c[i]);
+    }
+    cout << "Total AASCI value of string " << c << " is " << totalValue << "\n";
+    cout << "----- Character ASCII value -----" << "\n";
+    for (int i=0; i<c.length(); i++){
+        cout << c[i] << " : " << int(c[i]) << "\n";
+    }
+    return 0;
+}
