eab1b8216814998b417dad69204957b00e9f1d2c
Ananda Zuraenee Sudarmaji <97554212+zuraenee@users.noreply.github.com>
2023-02-25 21:48:11 +0000

    Update SumSquareDifference-Day12.java

--- SumSquareDifference-Day12.java
+++ SumSquareDifference-Day12.java
@@ -1,5 +1,5 @@
 public class sumSquareDifference {
-    public static void main(String[] args){
+    public static void sumSquareDifference(String[] args){
         int sumOfSquares = 0;
         int squareOfSum = 0, sum =0;
         for(int i=1; i<=100; i++){
