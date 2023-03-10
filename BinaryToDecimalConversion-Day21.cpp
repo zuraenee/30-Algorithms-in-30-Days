8cd3c3fc724570e40f48fee330bb223e5c2de82a
Ananda Zuraenee Sudarmaji <97554212+zuraenee@users.noreply.github.com>
2023-03-10 23:59:23 +0000

    Update and rename !!!BinaryToDecimalConversion-Day21.cpp to BinaryToDecimalConversion-Day21.cpp

---
+++ BinaryToDecimalConversion-Day21.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  long long n;
+  cout << "Enter a binary number: ";
+  cin >> n;
+  cout << n << " in binary = " << convertion(n) << " in decimal";
+  return 0;
+}
+
+int convertion(long long);
+
+int convertion(long long n) {
+  int decimal = 0, i = 0, reminder;
+
+  while (n!=0) {
+    reminder = n % 10;
+    n /= 10;
+    decimal += reminder * pow(2, i);
+    ++i;
+  }
+
+  return decimal;
+}
