2c7b7a84ef549ef59abfa1300c87398fa34bc0fc
Ananda Zuraenee Sudarmaji <97554212+zuraenee@users.noreply.github.com>
2023-05-09 14:30:57 +0100

    Create FactorialRecursion-Day27.cpp

---
+++ FactorialRecursion-Day27.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int num);
+
+int main() {
+
+  int num;
+
+  cout << "Enter an integer larger than 0: ";
+  cin >> num;
+
+  cout << "Factorial of " << num << " = " << factorial(num);
+
+  return 0;
+}
+
+int factorial(int num) {
+  if(num > 1)
+    return num * factorial(num - 1);
+  else
+    return 1;
+}
